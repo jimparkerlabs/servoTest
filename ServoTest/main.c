9e3da0b56489a883fd6dfa4e2c36819ce1e4a3da
little Rock Jim <littlerockapps@gmail.com>
2015-06-22 10:10:34 -0500

    first actaul stab.  No servo connected yet, but tracking time an LEDs (flashing every second, according to my logic).  If timing is off, the flashes will get out of sync with stopwatch.

--- ServoTest/main.c
+++ ServoTest/main.c
@@ -6,6 +6,7 @@
  */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include "pinDefines.h"
 #include "USART.h"
@@ -36,10 +37,33 @@ uint8_t newButtonPress(void) {
 	return ((pressed == 1) && (oldState == 0)); // pressed now, not before
 }
 
+volatile int trainTimer;
+volatile int pulseTimer;
+volatile int totalTime;
+ISR(TIMER0_COMPA_vect) {
+	trainTimer -=10;
+	pulseTimer -=10;
+	totalTime += 10;
+}
+
 int main(void) {
 	int period = 20000;
-	int width = 1500;
-	int step = 10;
+	int pulseWidth = 1500;
+	int testingPulseStep = 10;
+
+	// the general idea is to set up a timer to interrupt ever 10 us and count down
+	//  these 2 timers in the ISR.  the main()loop wil check to see when they are 0
+	//  and act accordingly.
+	trainTimer = period;
+	pulseTimer = pulseWidth;
+	totalTime = 0;
+
+	int elapsedTime = 0;
+	// at 8 Mhz, a prescaler of /8 and a compare value =10 should work.  Although, this might be too
+	// fast for me to process. 80 clock cycles is pretty small...
+	TCCR0A = 0b00000010 ; // ctc mode
+	TCCR0B = 0b00000010 ; // divide by 8
+	TIMSK0 = (1 << OCIE0A) ; // enable interrupt on match
 
 	DDRD &= ~(1 << PD6);    // set button port to input
 	PORTD |= (1 << PD6);  // enable pull-up resistor
@@ -48,40 +72,35 @@ int main(void) {
 
 	char serialCharacter;
 
-	LED_DDR = 0xff;
+	LED_DDR = 0xff;         // configure LED port
 	LED_PORT = 0b01010101;
 
 	initUSART();
-	transmitByte(0x0d);  //  cr
-	transmitByte(0x0a);  // lf
-	printString("Hello world!");
-	transmitByte(0x0d);  //  cr
-	transmitByte(0x0a);  // lf
-	printString("press '+' and '-' to move servo");
-	transmitByte(0x0d);  //  cr
-	transmitByte(0x0a);  // lf
 
+	printString("\r\nHello world!\r\n");
+	printString("press '+' and '-' to move servo\r\n");
+
+	sei();
 	while (1) {
 		serialCharacter = getByte();
 		if (serialCharacter == '+') {
-			width += step;
-			printString("new width: ");
-			//printString(itoa(width));
-			transmitByte(0x0d);  //  cr
-			transmitByte(0x0a);  // lf
+			pulseWidth += testingPulseStep;
+			//printString("new width: %i\r\n", pulseWidth);
 		}
 		if (serialCharacter == '-') {
-			width -= step;
-			printString("new width: ");
-			//printString(itoa(width));
-			transmitByte(0x0d);  //  cr
-			transmitByte(0x0a);  // lf
+			pulseWidth -= testingPulseStep;
+			//printString("new width: %i\r\n", pulseWidth);
 		}
 
-		PORTD |= (1 << PD5);  // start pulse
-		_delay_us(width);
-		PORTD &= ~(1 << PD5);    // end pulse
-		_delay_us(period - width);
+		if(pulseTimer == 0) {pulseTimer = pulseWidth; PORTD &= (1 << PD5);}
+		if(trainTimer == 0) {trainTimer = period; PORTD |= (1 << PD5);}
+
+		if(totalTime == 1000000) {
+			totalTime = 0;
+			elapsedTime += 1;
+			LED_PORT ^= 0x11111111;
+		}
+
 	}
 	return(0);
 }
